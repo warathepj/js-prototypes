2cade1b8d5487df37e71740f354eaea632247d48
warathepj <80021220+warathepj@users.noreply.github.com>
2021-07-08 16:57:59 +0700

    6 02

--- prototypes.js
+++ prototypes.js
@@ -1,7 +1,17 @@
-function personFactory(firstName, lastName) {
-    return {
-        firstName,
-        lastName,
-
+let personPrototype = {
+    greet: function (person) {
+        return "Hello, " + person.firstName;
     }
+};
+
+function personFactory(firstName, lastName) {
+    let person = Object.create(personPrototype, {
+        firstName: {writeable: false, value: firstName},
+        lastName: {writeable: false, value: lastName}
+    
+    });
+        return person;
 }
+
+let johnDoe = personFactory("John", "Doe");
+let janeSmith = personFactory("Jane", "Smith");
